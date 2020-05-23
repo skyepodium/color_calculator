f6ba02bd1689dbba232c9c8f501c11db3dc6d5bb
skyepodium <skyepodium@gmail.com>
2020-05-23 23:49:13 +0900

    first commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="back">
+    <div class="wrapper">
+      <input
+        type="text"
+        class="number"
+        readonly
+      >
+      <div class="pad_wrapper">
+        <div class="pad_line">
+          <div class="box box_2 clear top">
+            c
+          </div>
+          <div class="box box_1 top">
+            /
+          </div>
+          <div class="box box_1 top">
+            *
+          </div>
+        </div>
+        <div class="pad_line">
+          <div class="box box_1">
+            7
+          </div>
+          <div class="box box_1">
+            8
+          </div>
+          <div class="box box_1">
+            9
+          </div>
+          <div class="box box_1">
+            *
+          </div>
+        </div>
+        <div class="pad_line">
+          <div class="box box_1">
+            4
+          </div>
+          <div class="box box_1">
+            5
+          </div>
+          <div class="box box_1">
+            6
+          </div>
+          <div class="box box_1">
+            -
+          </div>
+        </div>
+        <div class="pad_line">
+          <div class="box box_1">
+            1
+          </div>
+          <div class="box box_1">
+            2
+          </div>
+          <div class="box box_1">
+            3
+          </div>
+          <div class="box box_1">
+            +
+          </div>
+        </div>  
+        <div class="pad_line">
+          <div class="box box_1">
+            0
+          </div>
+          <div class="box box_1">
+            00
+          </div>
+          <div class="box box_1">
+            .
+          </div>
+          <div class="box box_1 result">
+            =
+          </div>
+        </div>                
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import 'normalize.css'
+import '@/styles/index.css'
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
